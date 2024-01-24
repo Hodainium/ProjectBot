f5a7c9565d557ea26a38c3bc8ef53dc6981d1f4d
Hodainium <119259951+Hodainium@users.noreply.github.com>
2024-01-24 13:24:29 -0500

    Just added new playerChar. Also made new iconGlow. Now beginning to refactor ASC to live on PlayerChar

---
+++ Source/HereWeGo/Actors/Characters/HPlayerCharacterNew.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Characters/HPlayerCharacterNew.h"
+
+// Sets default values
+AHPlayerCharacterNew::AHPlayerCharacterNew()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AHPlayerCharacterNew::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHPlayerCharacterNew::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AHPlayerCharacterNew::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
