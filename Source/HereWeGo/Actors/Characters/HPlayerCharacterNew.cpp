84cac2e71959a47e93e18820c65c7f725cbda491
Hodainium <119259951+Hodainium@users.noreply.github.com>
2024-01-24 13:29:34 -0500

    Compiles

--- Source/HereWeGo/Actors/Characters/HPlayerCharacterNew.cpp
+++ Source/HereWeGo/Actors/Characters/HPlayerCharacterNew.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Actors/Characters/HPlayerCharacterNew.h"
+#include "HPlayerCharacterNew.h"
 
 // Sets default values
 AHPlayerCharacterNew::AHPlayerCharacterNew()
