e3f386bea7ca7241d82fa7b94dbab09bbf45b215
Hodainium <119259951+Hodainium@users.noreply.github.com>
2024-03-27 21:40:14 -0400

    Fixed maingamemenu ensure. Needed to remove auto activate

---
+++ Source/HereWeGo/AttributeSets/HWeaponAttributeSet.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HWeaponAttributeSet.h"
+
+#include "Net/UnrealNetwork.h"
+
+void UHWeaponAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UHWeaponAttributeSet, WeaponDamage, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UHWeaponAttributeSet, WeaponFireRate, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UHWeaponAttributeSet, WeaponRange, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UHWeaponAttributeSet, WeaponAccuracy, COND_None, REPNOTIFY_Always);
+}
+
+UHWeaponAttributeSet::UHWeaponAttributeSet()
+	: WeaponDamage(0.f)
+	, WeaponFireRate(0.f)
+	, WeaponRange(0.f)
+	, WeaponAccuracy(0.f)
+{
+}
+
+void UHWeaponAttributeSet::OnRep_WeaponDamage(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UHWeaponAttributeSet, WeaponDamage, OldValue);
+
+	// Call the change callback, but without an instigator
+	// This could be changed to an explicit RPC in the future
+	OnWeaponDamageChanged.Broadcast(nullptr, nullptr, nullptr, GetWeaponDamage() - OldValue.GetCurrentValue(), OldValue.GetCurrentValue(), GetWeaponDamage());
+}
+
+void UHWeaponAttributeSet::OnRep_WeaponFireRate(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UHWeaponAttributeSet, WeaponFireRate, OldValue);
+
+	// Call the change callback, but without an instigator
+	// This could be changed to an explicit RPC in the future
+	OnWeaponFireRateChanged.Broadcast(nullptr, nullptr, nullptr, GetWeaponFireRate() - OldValue.GetCurrentValue(), OldValue.GetCurrentValue(), GetWeaponFireRate());
+}
+
+void UHWeaponAttributeSet::OnRep_WeaponRange(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UHWeaponAttributeSet, WeaponRange, OldValue);
+
+	// Call the change callback, but without an instigator
+	// This could be changed to an explicit RPC in the future
+	OnWeaponRangeChanged.Broadcast(nullptr, nullptr, nullptr, GetWeaponRange() - OldValue.GetCurrentValue(), OldValue.GetCurrentValue(), GetWeaponRange());
+}
+
+void UHWeaponAttributeSet::OnRep_WeaponAccuracy(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UHWeaponAttributeSet, WeaponAccuracy, OldValue);
+
+	// Call the change callback, but without an instigator
+	// This could be changed to an explicit RPC in the future
+	OnWeaponAccuracyChanged.Broadcast(nullptr, nullptr, nullptr, GetWeaponAccuracy() - OldValue.GetCurrentValue(), OldValue.GetCurrentValue(), GetWeaponAccuracy());
+}
---
+++ Source/HereWeGo/AttributeSets/HWeaponAttributeSet.h
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "HAttributeSet.h"
+#include "HWeaponAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HEREWEGO_API UHWeaponAttributeSet : public UHAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+
+	UHWeaponAttributeSet();
+
+	ATTRIBUTE_ACCESSORS(UHWeaponAttributeSet, WeaponDamage);
+	ATTRIBUTE_ACCESSORS(UHWeaponAttributeSet, WeaponFireRate);
+	ATTRIBUTE_ACCESSORS(UHWeaponAttributeSet, WeaponRange);
+	ATTRIBUTE_ACCESSORS(UHWeaponAttributeSet, WeaponAccuracy);
+
+	//ATTRIBUTE_ACCESSORS(UHWeaponAttributeSet, Element);
+
+	// Delegate when WeaponDamage changes, some information may be missing on the client
+	mutable FHAttributeEvent OnWeaponDamageChanged;
+
+	// Delegate when FireRate changes
+	mutable FHAttributeEvent OnWeaponFireRateChanged;
+
+	// Delegate when range changes
+	mutable FHAttributeEvent OnWeaponRangeChanged;
+
+	// Delegate when accuracy changes, some information may be missing on the client
+	mutable FHAttributeEvent OnWeaponAccuracyChanged;
+
+protected:
+
+	UFUNCTION()
+	void OnRep_WeaponDamage(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	void OnRep_WeaponFireRate(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	void OnRep_WeaponRange(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	void OnRep_WeaponAccuracy(const FGameplayAttributeData& OldValue);
+
+
+private:
+
+	// The current damage attribute
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_WeaponDamage, Category = "H|WeaponDamage", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData WeaponDamage;
+
+	// The current firerate attribute
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_WeaponFireRate, Category = "H|WeaponFireRate", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData WeaponFireRate;
+
+	// The current range attribute
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_WeaponRange, Category = "H|WeaponRange", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData WeaponRange;
+
+	// The current accuracy attribute
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_WeaponAccuracy, Category = "H|WeaponAccuracy", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData WeaponAccuracy;
+};
